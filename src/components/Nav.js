3028093cebb185f08d69915d9b13d90724d8d8a5
Ali-404 <abdellali.aitaddi@gmail.com>
2024-11-14 20:05:56 +0100

    adding useLocation

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import { useLocation } from 'react-router-dom'
 
 export default function Nav() {
+
+  const location = useLocation()
+  
   return (
     <nav className='navbar navbar-expand text-bg-primary p-2 shadow w-100  top-0 '>
       <h1 className='navbar-brand text-light'>Routing Exemple </h1>
+      <span>{location.pathname}</span>
     </nav>
   )
 }
